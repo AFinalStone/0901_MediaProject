9794456fb83e56d0a00a23d0bccca4a1c769a5ea
afinalstone <afinalstone@foxmail.com>
2022-04-23 22:28:19 +0800

    add: 添加OpenGl模块

---
+++ 0401OpenGL/src/main/java/com/example/opengl/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.opengl;
+
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+public class MainActivity extends AppCompatActivity {
+
+    GLSurfaceView mGLSurfaceView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mGLSurfaceView = findViewById(R.id.gl_surface_view);
+        mGLSurfaceView.setRenderer(new GLSurfaceView.Renderer() {
+            @Override
+            public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
+
+            }
+
+            @Override
+            public void onSurfaceChanged(GL10 gl10, int i, int i1) {
+
+            }
+
+            @Override
+            public void onDrawFrame(GL10 gl10) {
+
+            }
+        });
+    }
+}
